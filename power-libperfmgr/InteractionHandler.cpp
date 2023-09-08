PA8953/android_device_bq_msm8953-common

msm8953-common: power: Update power HAL to version 1.3
This adds support for the EXPENSIVE_RENDERING power hint.

BUG: 110112323
Test: adb shell cat /sys/class/kgsl/kgsl-3d0/devfreq/cur_freq
      Verified GPU frequency reset by adb shell pkill surfaceflinger and system_server
Change-Id: Ia1f6e99b67c5e88b610412ad30f152d241095a48
Merged-In: Ia1f6e99b67c5e88b610412ad30f152d241095a48

power-libperfmgr/InteractionHandler.cpp
@@ -16,7 +16,7 @@
 
 //#define LOG_NDEBUG 0
 
-#define LOG_TAG "android.hardware.power@1.2-service.msm8953-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.msm8953-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>

power-libperfmgr/Power.cpp
@@ -15,7 +15,7 @@
  */
 
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
-#define LOG_TAG "android.hardware.power@1.2-service.msm8953-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.msm8953-libperfmgr"
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -37,7 +37,7 @@ extern struct stat_pair rpm_stat_map[];
 namespace android {
 namespace hardware {
 namespace power {
-namespace V1_2 {
+namespace V1_3 {
 namespace implementation {
 
 using ::android::hardware::power::V1_0::Feature;
@@ -89,6 +89,12 @@ Power::Power() :
                             if (state == "LOW_LATENCY") {
                                 ALOGI("Initialize with AUDIO_LOW_LATENCY on");
                                 mHintManager->DoHint("AUDIO_LOW_LATENCY");
+                            }
+
+                            state = android::base::GetProperty(kPowerHalRenderingProp, "");
+                            if (state == "EXPENSIVE_RENDERING") {
+                                ALOGI("Initialize with EXPENSIVE_RENDERING on");
+                                mHintManager->DoHint("EXPENSIVE_RENDERING");
                             }
                             // Now start to take powerhint
                             mReady.store(true);
@@ -437,6 +443,39 @@ Return<void> Power::powerHintAsync_1_2(PowerHint_1_2 hint, int32_t data) {
     return Void();
 }
 
+// Methods from ::android::hardware::power::V1_3::IPower follow.
+Return<void> Power::powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) {
+    if (!isSupportedGovernor() || !mReady) {
+        return Void();
+    }
+
+    if (hint == PowerHint_1_3::EXPENSIVE_RENDERING) {
+        if (mVRModeOn || mSustainedPerfModeOn) {
+            ALOGV("%s: ignoring due to other active perf hints", __func__);
+            return Void();
+        }
+
+        if (data > 0) {
+            ATRACE_INT("EXPENSIVE_RENDERING", 1);
+            mHintManager->DoHint("EXPENSIVE_RENDERING");
+            if (!android::base::SetProperty(kPowerHalRenderingProp, "EXPENSIVE_RENDERING")) {
+                ALOGE("%s: could not set powerHAL rendering property to EXPENSIVE_RENDERING",
+                      __func__);
+            }
+        } else {
+            ATRACE_INT("EXPENSIVE_RENDERING", 0);
+            mHintManager->EndHint("EXPENSIVE_RENDERING");
+            if (!android::base::SetProperty(kPowerHalRenderingProp, "")) {
+                ALOGE("%s: could not clear powerHAL rendering property", __func__);
+            }
+        }
+    } else {
+        return powerHintAsync_1_2(static_cast<PowerHint_1_2>(hint), data);
+    }
+
+    return Void();
+}
+
 constexpr const char* boolToString(bool b) {
     return b ? "true" : "false";
 }
@@ -464,7 +503,7 @@ Return<void> Power::debug(const hidl_handle& handle, const hidl_vec<hidl_string>
 }
 
 }  // namespace implementation
-}  // namespace V1_2
+}  // namespace V1_3
 }  // namespace power
 }  // namespace hardware
 }  // namespace android

power-libperfmgr/power-helper.c
@@ -28,7 +28,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@1.2-service.msm8953-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.msm8953-libperfmgr"
 
 #include <errno.h>
 #include <inttypes.h>

power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.msm8953-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.msm8953-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -30,11 +30,11 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::power::V1_2::IPower;
-using android::hardware::power::V1_2::implementation::Power;
+using android::hardware::power::V1_3::IPower;
+using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char** /* argv */) {
-    ALOGI("Power HAL Service 1.2 is starting.");
+    ALOGI("Power HAL Service 1.3 is starting.");
 
     android::sp<IPower> service = new Power();
     if (service == nullptr) {

cheldump/device_xiaomi_msm8953-common

power-libperfmgr/InteractionHandler.cpp
@@ -16,7 +16,7 @@
 
 //#define LOG_NDEBUG 0
 
-#define LOG_TAG "android.hardware.power@1.2-service.onclite-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.onclite-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>

power-libperfmgr/Power.cpp
@@ -15,7 +15,7 @@
  */
 
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
-#define LOG_TAG "android.hardware.power@1.2-service.onclite-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.onclite-libperfmgr"
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -37,7 +37,7 @@ extern struct stat_pair rpm_stat_map[];
 namespace android {
 namespace hardware {
 namespace power {
-namespace V1_2 {
+namespace V1_3 {
 namespace implementation {
 
 using ::android::hardware::power::V1_0::Feature;
@@ -89,6 +89,12 @@ Power::Power() :
                             if (state == "LOW_LATENCY") {
                                 ALOGI("Initialize with AUDIO_LOW_LATENCY on");
                                 mHintManager->DoHint("AUDIO_LOW_LATENCY");
+                            }
+
+                            state = android::base::GetProperty(kPowerHalRenderingProp, "");
+                            if (state == "EXPENSIVE_RENDERING") {
+                                ALOGI("Initialize with EXPENSIVE_RENDERING on");
+                                mHintManager->DoHint("EXPENSIVE_RENDERING");
                             }
                             // Now start to take powerhint
                             mReady.store(true);
@@ -437,6 +443,39 @@ Return<void> Power::powerHintAsync_1_2(PowerHint_1_2 hint, int32_t data) {
     return Void();
 }
 
+// Methods from ::android::hardware::power::V1_3::IPower follow.
+Return<void> Power::powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) {
+    if (!isSupportedGovernor() || !mReady) {
+        return Void();
+    }
+
+    if (hint == PowerHint_1_3::EXPENSIVE_RENDERING) {
+        if (mVRModeOn || mSustainedPerfModeOn) {
+            ALOGV("%s: ignoring due to other active perf hints", __func__);
+            return Void();
+        }
+
+        if (data > 0) {
+            ATRACE_INT("EXPENSIVE_RENDERING", 1);
+            mHintManager->DoHint("EXPENSIVE_RENDERING");
+            if (!android::base::SetProperty(kPowerHalRenderingProp, "EXPENSIVE_RENDERING")) {
+                ALOGE("%s: could not set powerHAL rendering property to EXPENSIVE_RENDERING",
+                      __func__);
+            }
+        } else {
+            ATRACE_INT("EXPENSIVE_RENDERING", 0);
+            mHintManager->EndHint("EXPENSIVE_RENDERING");
+            if (!android::base::SetProperty(kPowerHalRenderingProp, "")) {
+                ALOGE("%s: could not clear powerHAL rendering property", __func__);
+            }
+        }
+    } else {
+        return powerHintAsync_1_2(static_cast<PowerHint_1_2>(hint), data);
+    }
+
+    return Void();
+}
+
 constexpr const char* boolToString(bool b) {
     return b ? "true" : "false";
 }
@@ -464,7 +503,7 @@ Return<void> Power::debug(const hidl_handle& handle, const hidl_vec<hidl_string>
 }
 
 }  // namespace implementation
-}  // namespace V1_2
+}  // namespace V1_3
 }  // namespace power
 }  // namespace hardware
 }  // namespace android

power-libperfmgr/power-helper.c
@@ -28,7 +28,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@1.2-service.onclite-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.onclite-libperfmgr"
 
 #include <errno.h>
 #include <inttypes.h>

power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.onclite-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.onclite-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -30,11 +30,11 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::power::V1_2::IPower;
-using android::hardware::power::V1_2::implementation::Power;
+using android::hardware::power::V1_3::IPower;
+using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char** /* argv */) {
-    ALOGI("Power HAL Service 1.2 is starting.");
+    ALOGI("Power HAL Service 1.3 is starting.");
 
     android::sp<IPower> service = new Power();
     if (service == nullptr) {